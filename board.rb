51a477e5770ea7aea8970b3ac284ad9afffe91e7
Oscar Alvarez & Francis Rocco <oscar.alvarez.8830@gmail.com>
2016-06-16 16:17:34 -0400

    build create grid, call in run

--- board.rb
+++ board.rb
@@ -1,12 +1,30 @@
 class Board
+  attr_reader :size
+  attr_accessor :grid
 
+  def initialize(size = 9)
+    @size = size
+  end
 
-  def initialize
-
+  def create_grid(size)
+    @grid = Array.new(size) { Array.new(size) { Tile.new(self) } }
   end
 
   def populate
+    num_bombs = ( size ** 2 ) / 4
+    num_bombs.times do
 
+    end
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, input)
+    row, col = pos
+    grid[row][col] = input
   end
 
   def render
---
+++ minesweeper.rb
@@ -0,0 +1,15 @@
+class Minesweeper
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def run
+    board.create_grid
+    board.populate
+    play until game_over?
+
+  end
+
+end
